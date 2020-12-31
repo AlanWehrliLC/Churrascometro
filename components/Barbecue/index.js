42b4c14606dfe66e9fb80d13d48da306cb0439eb
AlanWehrliLC <AlanWehrliLC@users.noreply.github.com>
2020-12-31 03:22:41 -0300

    style

--- components/Barbecue/index.js
+++ components/Barbecue/index.js
@@ -83,7 +83,7 @@ return(
         <input className='inputGlobal' value={kid} onChange={(e)=>setKid(e.target.value)} name='kids' type='number' placeholder='Kids'  />
         <input className='inputGlobal' value={duration} onChange={(e)=>setDuration(e.target.value)} name='duratio' type='number' placeholder='Duratio'  />
         <input className='calculationGlobal' type='button'  onClick={calculation} value='Calculation' />
-        {resultado}
+        <div id='textBarbecue' >{resultado}</div>
       </div>
 )
 }
