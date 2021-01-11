b279af1383e00c2cf9bdb9c55fa061370c9dcf60
AlanWehrliLC <AlanWehrliLC@users.noreply.github.com>
2021-01-11 08:27:28 -0300

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 import '../styles/styles.css'
-import HeadPage from '../components/HeadPage'
-import HeaderPage from '../components/HeaderPage'
-import FooterPage from '../components/FooterPage'
 
 
 function MyApp({ Component, pageProps }) {
     return (
-      <div>
-        <HeadPage title='Churrascometro' />
-        <HeaderPage />
+      <>
         <Component {...pageProps} />
-        <FooterPage />
-      </div>
+      </>
     )
 }
 export default  MyApp
